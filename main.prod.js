dba6f2efe1387dc13f158fba94ab5f1c42b604e4
potapovDim <dmytrop@weblium.com>
2017-02-19 14:27:19 +0200

    set up electron

---
+++ main.prod.js
@@ -0,0 +1,18 @@
+"use strict"
+const template = '/index.production.html'
+const app = require('app')
+const BrowserWindow = require('browser-window')
+let mainWindow = null
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL('file://' + __dirname + template)
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
